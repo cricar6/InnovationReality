e0977c274165f147dc5635efad2600a93af67a76
cristian6569 <cristian6569@gmail.com>
2019-08-26 14:10:26 -0500

    Updates

---
+++ precache-manifest.d6da7af346c448e731bc3ee9f8f7bb82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e29ec0e5336d30f7f21100bb2b8f78",
+    "url": "/InnovationReality/index.html"
+  },
+  {
+    "revision": "13d889babf04350b7c3b",
+    "url": "/InnovationReality/static/css/main.b1766830.chunk.css"
+  },
+  {
+    "revision": "084d5b76ca21a99c037e",
+    "url": "/InnovationReality/static/js/2.c100bb8c.chunk.js"
+  },
+  {
+    "revision": "13d889babf04350b7c3b",
+    "url": "/InnovationReality/static/js/main.355f8326.chunk.js"
+  },
+  {
+    "revision": "81c81a7fb8b035282592",
+    "url": "/InnovationReality/static/js/runtime~main.3169bc8e.js"
+  }
+]);
